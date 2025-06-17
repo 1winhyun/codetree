454415e9475228c8ceff772f3b10d47f32309aca
seunghyun <140399766+1winhyun@users.noreply.github.com>
2025-06-17 17:37:45 +0900

    Title: 사칙연산 연습, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250617/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 30;
+        int c = a+b;
+        System.out.println(c/8);
+        System.out.println(c%8);
+    }
+}
