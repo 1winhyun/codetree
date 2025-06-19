d9a3c8878bd7516ec880900648749b7cdda6a23a
seunghyun <140399766+1winhyun@users.noreply.github.com>
2025-06-19 23:54:16 +0900

    Title: 점수 입출력, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250619/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        System.out.println("Your score is "+ score + " point.");
+    }
+}
