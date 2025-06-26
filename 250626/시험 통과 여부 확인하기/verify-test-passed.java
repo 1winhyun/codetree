0c877af75338e2555cc4f26712236036d561696b
seunghyun <140399766+1winhyun@users.noreply.github.com>
2025-06-26 23:43:01 +0900

    Title: 시험 통과 여부 확인하기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250626/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        if(N >= 80){
+            System.out.println("pass");
+        }
+        else{
+            System.out.println((80-N) + " more score");
+        }
+    }
+}
